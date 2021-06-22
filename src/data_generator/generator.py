ffa962be4d27e2ab680b52789d0eba2e197fdb87
thepooons <poons.saitama@gmail.com>
2021-06-22 19:02:37 +0530

    add: data-gen-revamp

--- src/data_generator/generator.py
+++ src/data_generator/generator.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from pandas.core import generic
 import yaml
 from itertools import combinations
-
+from tqdm import tqdm
 
 class ClickStreamGen(object):
     def __init__(
@@ -11,10 +12,6 @@ class ClickStreamGen(object):
         clickstream_data_path,
         clustered_article_data_path
     ):
-        self.clickstream_data = pd.DataFrame(
-            columns=["user_id", "session_id", "article_id",
-                "rank", "click", "time_spent"]
-        )
         self.num_users = num_users
         self.clickstream_data_path = clickstream_data_path
         self.clustered_article_data = pd.read_csv(clustered_article_data_path)
@@ -29,38 +26,43 @@ class ClickStreamGen(object):
             for article_id in cluster_articles:
                 article_prob_hash[article_id] = 1
             cluster_article_prob_hash[cluster_id] = article_prob_hash
+
         self.cluster_article_prob_hash = cluster_article_prob_hash
 
     def generate(self):
         # generate lists
-        self.user_ids, self.session_ids, self.user_num_sessions_hash = self._generate_user_id_session_ids()
+        self.user_ids, self.session_ids, self.user_num_sessions_hash = self._generate_user_ids_session_ids()
         self.article_ids = self._generate_article_ids()
         self.ranks = self._generate_rank()
-        
+        self.click, self.time_spent = self._generate_click_time_spent()
+
         # test lists 
         lists_hash = {
-            "user_ids": self.user_ids,
-            "session_ids": self.session_ids,
-            "article_ids": self.article_ids,
-            "ranks": self.ranks,
-
+            "user_id": self.user_ids,
+            "session_id": self.session_ids,
+            "article_id": self.article_ids,
+            "rank": self.ranks,
+            "click": self.click,
+            "time_spent": self.time_spent
         }
         for list1, list2 in combinations(lists_hash.keys(), 2):    
             assert len(lists_hash[list1]) == len(lists_hash[list2]), \
                 f"length of {list1} ({len(lists_hash[list1])}) and {list2} ({len(lists_hash[list2])}) doesn't match"
         
         # save clickstream data
-        print(len(self.article_ids), len(np.unique(self.article_ids)))
+        generated_data = pd.DataFrame(lists_hash)
+        generated_data.to_csv(self.clickstream_data_path, index=False)
 
-    def _generate_user_id_session_ids(self):
+    def _generate_user_ids_session_ids(self):
         """generate a list of session ids spanning `self.num_users`
         """
         user_ids = []
         session_ids = []
         user_num_sessions_hash = {}
-        for user_id in range(self.num_users):
+        for user_id in range(1, self.num_users + 1):
             user_num_sessions_hash[user_id] = 0
-            num_sessions = 5 #TODO: sample this value from a distribution
+            num_sessions = int(np.random.normal(loc=10, scale=2)) #random
+            num_sessions = max(1, num_sessions)
             for session_id in range(1, num_sessions + 1):
                 user_ids.extend([user_id] * 10)
                 session_ids.extend([session_id] * 10) # 10 -> num articles recommended
@@ -75,7 +77,10 @@ class ClickStreamGen(object):
         article_ids = []
         for user_id in self.user_num_sessions_hash.keys():
             num_sessions = self.user_num_sessions_hash[user_id]
-            user_preference_cluster_id = 3 #TODO: sample this from a uniform distribution
+            random_uniform_gen = np.random.default_rng()
+            user_preference_cluster_id = random_uniform_gen.choice(
+                list(self.cluster_article_prob_hash.keys())
+            ) #random
 
             user_preference_article_ids = ClickStreamGen.get_cluster_article_ids(
                 cluster_article_prob_hash=self.cluster_article_prob_hash,
@@ -120,8 +125,40 @@ class ClickStreamGen(object):
     def _generate_click_time_spent(self):
         """generate binary vector, real numbered vector of length 10 for each session for each user
         """
-        pass
+        click = []
+        time_spent = []
+        generator = tqdm(
+            zip(
+                self.user_ids, 
+                self.session_ids, 
+                self.article_ids, 
+                self.ranks
+            ), 
+            desc="generating time_spent"
+        )
+        for data in generator:
+            #NOTE: not the best way to generate time_spent
+            user_id    = data[0]
+            session_id = data[1]
+            article_id = data[2]
+            rank       = data[3]
+            flesch_kincaid_score = self.clustered_article_data.loc[
+                self.clustered_article_data.loc[:, "article_id"] == article_id,
+                "flesch_kincaid_grade_score"
+            ].values
+            random_uniform_gen = np.random.default_rng()
+            is_click = random_uniform_gen.choice(
+                a=[1, 0],
+                p=[0.7, 0.3]
+            )
+            if is_click:
+                click.append(1)
+                time_spent.append(flesch_kincaid_score[0])
+            else:
+                click.append(0)
+                time_spent.append(0)
 
+        return click, time_spent
 
 
 if __name__ == "__main__":
